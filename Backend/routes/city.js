52c31366ce8dead1964d011879e69f1ca9185424
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-06-19 11:20:36 +0100

    connected the server to mongo db and created skeleton for all CRUD routes

---
+++ Backend/routes/city.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const router = express.Router()
+
+//Create
+
+router.post('/', async (req, res) => {
+
+});
+
+//Read
+
+router.get('/:id', async (req, res) => {
+
+});
+
+//Read All
+
+router.get('/', async (req, res) => {
+
+});
+
+//Update
+
+router.patch('/:id', async (req, res) => {
+
+});
+
+//Delete
+
+router.delete('/:id', async (req, res) => {
+
+});
+
+module.exports = router
---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
+
+db.once('open', () => {
+    console.log('Connected to Database')
+});
+
+db.on('error', (error) => {
+    console.log(error)
+});
+
+app.use(express.json());
+
+const cityRouter = require('./routes/city')
+app.use('/home', cityRouter)
+
+
+const PORT = 3000;
+app.listen(PORT, () => 
+console.log(`Server started on port ${PORT}`)
+);
