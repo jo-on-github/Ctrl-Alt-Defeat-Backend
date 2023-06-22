3a6704d1d012d56a3bd5d9e454724d467a18cc63
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-06-22 13:12:49 +0100

    created user schema

---
+++ Backend/Models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({ 
+
+    email: {
+        type: String,
+        required: true
+    },
+    
+    firstName: {
+        type: String,
+        required: true
+    },
+
+     surname: {
+        type: String,
+        required: true
+     },
+
+     dateOfBirth: {
+        type: Date,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    location: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
